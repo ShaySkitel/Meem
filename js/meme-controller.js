4cd6d992762bbdbf95163d38183ea51c60fdda25
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2022-12-14 11:41:15 +0200

    Change renderMeme to render current meme properly

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,22 +1,9 @@
 'use strict'
 
 let gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
-let gImgs = [{ id: 1, url: '../images/1.jpg', keywords: ['funny', 'cat'] }];
+
 let gElCanvas
 let gCtx
-
-let gMeme = {
-    selectedImgId: 5,
-    selectedLineIdx: 0,
-    lines: [
-        {
-            txt: 'I sometimes eat Falafel',
-            size: 20,
-            align: 'left',
-            color: 'red'
-        }
-    ]
-}
 
 function onInit() {
     gElCanvas = document.querySelector('.canvas-container canvas')
@@ -24,23 +11,31 @@ function onInit() {
 }
 
 function renderMeme() {
+    const meme = getMeme()
+    const memeImg = getMemeImg()
+
+    const txt = meme.lines[meme.selectedLineIdx].txt
+    const txtColor = meme.lines[meme.selectedLineIdx].color
+    const txtAlign = meme.lines[meme.selectedLineIdx].align
+    const txtSize = meme.lines[meme.selectedLineIdx].size
+
     const elImg = new Image()
-    elImg.src = gImgs[0].url
+    elImg.src = memeImg
 
     elImg.onload = () => {
         gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
-        renderTxt('Placeholder', gElCanvas.width / 2, gElCanvas.height / 2)
+        renderTxt({txt, txtColor, txtAlign, txtSize}, gElCanvas.width / 2, gElCanvas.height / 2)
     }
 }
 
-function renderTxt(text, x, y) {
+function renderTxt({txt, txtColor, txtAlign, txtSize}, x, y) {
     gCtx.lineWidth = 2
     gCtx.strokeStyle = 'black'
-    gCtx.fillStyle = 'white'
-    gCtx.font = '40px Impact'
-    gCtx.textAlign = 'center'
-    gCtx.textBaseline = 'middle'
+    gCtx.fillStyle = txtColor
+    gCtx.font = `${txtSize}px Impact`
+    gCtx.textAlign = txtAlign
+    gCtx.textBaseline = txtAlign
 
-    gCtx.fillText(text, x, y)
-    gCtx.strokeText(text, x, y)
+    gCtx.fillText(txt, x, y)
+    gCtx.strokeText(txt, x, y)
 }
--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -1,1 +1,24 @@
 'use strict'
+
+let gImgs = [{ id: 1, url: '../images/1.jpg', keywords: ['funny', 'cat'] }];
+
+let gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ]
+}
+
+function getMeme(){
+    return gMeme
+}
+
+function getMemeImg(){
+    return gImgs.find(img => img.id === gMeme.selectedImgId).url
+}
